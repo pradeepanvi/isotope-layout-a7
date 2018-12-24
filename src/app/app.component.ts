95e42c9ed806daa0d4275570c66699cb202d50e4
pradeepanvi <pradeepkumar.anvi@gmail.com>
2018-12-24 13:58:07 +0530

    working-with-a7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { timer } from "rxjs";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'isotope-layout';
+
+  ngOnInit(){
+    //This is just setTimeout not for use.
+    const numbers = timer(1000);
+    numbers.subscribe(() => {
+      console.log("Start now")
+    });
+  }
 }
